e32f301d898c65c8a8c07d03003625a5528bfde4
Todd Cornett <tcornett@gmail.com>
2023-09-08 23:46:21 -0700

    Updating the Examples to handle PHP8

--- APISample/SSOWithWSDL/App_Code/ConciergeApiHelper.php
+++ APISample/SSOWithWSDL/App_Code/ConciergeApiHelper.php
@@ -1,16 +1,12 @@
 <?php
-/*
- This class is used to create request and generate response.
- Created Date: 12/April/2013
- Created By: SmartdataInc.
-*/
+
 $filedir = dirname(__FILE__);
 
 define('base_path',$filedir);
 include_once('phpseclib/Crypt/RSA_XML.php');
 
 class ApiHelper{
-  
+
   var $AccesskeyId;
   var $AssociationId;
   var $SecretAccessKey;
@@ -18,9 +14,9 @@ class ApiHelper{
   var $PortalUserName;
   var $SigningCertificateId;
   var $DigitalSignature;
-  
+
   public function __construct(){
-  
+
   $this->AccesskeyId = null;
   $this->AssociationId = null;
   $this->SecretAccessKey = null;
@@ -28,40 +24,40 @@ class ApiHelper{
   $this->PortalUserName = null;
   $this->SigningCertificateId = null;
   $this->DigitalSignature = null;
-    
+
   }
-  
-  
-  
- // Generate Message Signature   
+
+
+
+ // Generate Message Signature
  public function MessageSignature($method='CreatePortalSecurityToken'){
-    
+
         $call  = "http://membersuite.com/contracts/IConciergeAPIService/$method";
-        
+
         $secret = base64_decode($this->SecretAccessKey);
         $AssociationId = $this->AssociationId;
         $SessionId = "";
         $data = "$call$AssociationId$SessionId";
-        
+
         return base64_encode(hash_hmac('sha1', $data, $secret, True));
-    
-    }    
-    
+
+    }
+
    // Generate Digital Signature
    public function DigitalSignature($rsaXML){
-    
+
         $xmlrsa = new Crypt_RSA_XML();
         $xmlrsa->loadKeyfromXML($rsaXML);
         $signature = $xmlrsa->sign($this->PortalUserName);
-        
+
         return base64_encode($signature);
-    
+
    }
-   
+
    //Generate Portal Security Token
    public function CreatePortalSecurityToken($filecontent){
-    
-    // Values that needs to be changed   
+
+    // Values that needs to be changed
     $replacearr = array('<To></To>',
                         '<AccessKeyId></AccessKeyId>',
                         '<AssociationId></AssociationId>',
@@ -70,7 +66,7 @@ class ApiHelper{
                         '<signingCertificateId></signingCertificateId>',
                         '<signature></signature>'
                         );
-         
+
     // By these values
     $replacevalue = array('<To>https://api.membersuite.com/</To>',
                           '<AccessKeyId>'.$this->AccesskeyId.'</AccessKeyId>',
@@ -80,19 +76,19 @@ class ApiHelper{
                           '<signingCertificateId>'.$this->SigningCertificateId.'</signingCertificateId>',
                           '<signature>'.$this->DigitalSignature.'</signature>'
                           );
-         
+
     // Replace strings
-         
+
     $apiheaderrequest = str_replace($replacearr,$replacevalue,$filecontent);
-    
+
     $soap = new SoapClient('https://api.membersuite.com/mex');
- 
+
     $response = $soap-> __doRequest($apiheaderrequest, 'https://api.membersuite.com', 'http://membersuite.com/contracts/IConciergeAPIService/CreatePortalSecurityToken', '1.0');
-    
+
     return $response;
 
   }
-    
+
 }
 
 ?>
